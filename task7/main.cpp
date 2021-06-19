37c717220ccffc5272c3a5a19cfe683eb5d12ba3
poyotamu000 <penelope_rx_104ff@yahoo.co.jp>
2021-06-19 23:52:20 +0900

    fix: dW and ddW

--- task7/main.cpp
+++ task7/main.cpp
@@ -78,8 +78,8 @@ void WdWddW_Rotation(
   const Eigen::Vector3d Rp = R*p;
   W = (Rp-q).squaredNorm();
   // compute gradient and hessian of the energy below.
-  dW = Skew(Rp) * (Rp - q) / W;
-  ddW = Skew(Rp) * (Eigen::Matrix3d::Identity() - (Rp - q) * (Rp - q).transpose() / (W * W)) * Skew(Rp) / W;
+  dW = 2 * Skew(Rp) * (Rp - q);
+  ddW = 2 * Skew(Rp) * (Eigen::Matrix3d::Identity() - (Rp - q) * (Rp - q).transpose() / (W * W)) * Skew(Rp);
   // ddW << 0,p[2]*2,-p[1]*2, -p[2]*2,0,p[0]*2, p[1]*2,-p[0]*2,0;
   // ddW << p[0]*2, p[1]*2, p[2]*2, p[0]*2, p[1]*2, p[2]*2, p[0]*2, p[1]*2, p[2]*2;
 }
